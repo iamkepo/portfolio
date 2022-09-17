deac5619fe67e6bba4dff05a7ca62f71c9021a03
iamkepo <christamour.kakpo@gmail.com>
2022-09-17 20:07:57 +0200

    beta exp

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,79 @@ import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
 
+const experiences = [
+  {
+    title: "Collaborations en entreprises",
+    projets: [
+      {
+        name: "Classe 19",
+        img: "url('./Tablette.png')",
+        link: "https://classe19.com/",
+        description: "Classe19 est une tablette éducative donnant un accès continu à tout un ensemble de ressources éducatives dans les matières principales (Mathématiques, PCT, SVT, Français et Anglais).",
+        date: "2020-2021"
+      },
+      {
+        name: "PharMap",
+        img: "url('./phone.png')",
+        link: "https://pharmap.co",
+        description: "PharMap est une appication qui te permet de trouve le médicament que tu cherches en un clic partout au Bénin",
+        date: "2021"
+      },
+      {
+        name: "Sentinelle",
+        img: "url('./sentinelle.webp')",
+        link: "",
+        description: "Visite technique, assurance, TVM et bien plus encore… voici autant de documents et pièces qu’il faut absolument garder à jour en tant que propriétaire de voiture.",
+        date: "2021"
+      },
+    ],
+  },
+  {
+    title: "Prèstations de services",
+    projets: [
+      {
+        name: "D2A",
+        img: "url('./d2a.png')",
+        link: "https://d2a.swiitch.design/",
+        description: "DU PROFESSIONNALISME AU SERVICE DU DROIT ET DE LA JUSTICE, Nous vous accompagnons dans la recherche de solutions à vos difficultés et anticipons sur vos situations heureuses ou moins favorables.",
+        date: "2020"
+      },
+    ],
+  },
+  {
+    title: "Projets personnels",
+    projets: [
+      {
+        name: "Pixels war",
+        img: "url('./pixelswar.png')",
+        link: "https://pixelswar.herokuapp.com/",
+        description: "Il s'agit d'un jeu web",
+        date: "2021-2022"
+      },
+      {
+        name: "World Cube",
+        img: "url('./w_cube.png')",
+        link: "https://iamkepo.github.io/w_cube/",
+        description: "Il s'agit d'un monde 3D en cube",
+        date: "2021-2022"
+      },
+      {
+        name: "World Sphère",
+        img: "url('./w_sphere.png')",
+        link: "https://iamkepo.github.io/w_sphere/",
+        description: "Il s'agit d'un monde 3D en sphère",
+        date: "2022"
+      },
+      {
+        name: "Mon portfolio",
+        img: "url('./portfolio.png')",
+        link: "https://iamkepo.github.io/portfolio/",
+        description: "Il s'agit de mon portfolio",
+        date: "2022"
+      },
+    ],
+  }
+];
 function App() {
   const [clickpage, setclickpage] = useState(window.location.pathname);
   
@@ -15,7 +88,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout clickpage={clickpage} />}>
           <Route index element={<Home ready={()=> setclickpage("")} />} />
-          <Route path="experiences/:group/:item" element={<Experiences ready={()=> setclickpage("experiences")} />} />
+          <Route 
+            path="experiences/:group/:item" 
+            element={<Experiences ready={()=> setclickpage("experiences")} experiences={experiences} />} 
+          />
           <Route path="services" element={<Services ready={()=> setclickpage("services")} />} />
           <Route path="contact" element={<Contact ready={()=> setclickpage("contact")} />} />
           <Route path="*" element={<NoPage ready={()=> setclickpage("404")} />} />
--- src/pages/Experiences.jsx
+++ src/pages/Experiences.jsx
@@ -1,110 +1,41 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+
 const Experiences = (props) => {
-  const [step, setstep] = useState(0);
-  
-  const experiences = [
-    {
-      title: "Collaborations en entreprises",
-      projets: [
-        {
-          name: "Classe 19",
-          img: "url('./Tablette.png')",
-          link: "https://classe19.com/",
-          description: "Classe19 est une tablette éducative donnant un accès continu à tout un ensemble de ressources éducatives dans les matières principales (Mathématiques, PCT, SVT, Français et Anglais).",
-          date: "2020-2021"
-        },
-        {
-          name: "PharMap",
-          img: "url('./phone.png')",
-          link: "https://pharmap.co",
-          description: "PharMap est une appication qui te permet de trouve le médicament que tu cherches en un clic partout au Bénin",
-          date: "2021"
-        },
-        {
-          name: "Sentinelle",
-          img: "url('./sentinelle.webp')",
-          link: "",
-          description: "Visite technique, assurance, TVM et bien plus encore… voici autant de documents et pièces qu’il faut absolument garder à jour en tant que propriétaire de voiture.",
-          date: "2021"
-        },
-      ],
-    },
-    {
-      title: "Prèstations de services",
-      projets: [
-        {
-          name: "D2A",
-          img: "url('./d2a.png')",
-          link: "https://d2a.swiitch.design/",
-          description: "DU PROFESSIONNALISME AU SERVICE DU DROIT ET DE LA JUSTICE, Nous vous accompagnons dans la recherche de solutions à vos difficultés et anticipons sur vos situations heureuses ou moins favorables.",
-          date: "2020"
-        },
-      ],
-    },
-    {
-      title: "Projets personnels",
-      projets: [
-        {
-          name: "Pixels war",
-          img: "url('./pixelswar.png')",
-          link: "https://pixelswar.herokuapp.com/",
-          description: "Il s'agit d'un jeu web",
-          date: "2021-2022"
-        },
-        {
-          name: "World Cube",
-          img: "url('./w_cube.png')",
-          link: "https://iamkepo.github.io/w_cube/",
-          description: "Il s'agit d'un monde 3D en cube",
-          date: "2021-2022"
-        },
-        {
-          name: "World Sphère",
-          img: "url('./w_sphere.png')",
-          link: "https://iamkepo.github.io/w_sphere/",
-          description: "Il s'agit d'un monde 3D en sphère",
-          date: "2022"
-        },
-        {
-          name: "Mon portfolio",
-          img: "url('./portfolio.png')",
-          link: "https://iamkepo.github.io/portfolio/",
-          description: "Il s'agit de mon portfolio",
-          date: "2022"
-        },
-      ],
-    }
-  ];
+
+  const [step, setstep] = useState([]);
 
   const params = useParams();
-  
+
   useEffect(()=>{
     props.ready();
-    let stock = 0;
-    experiences.forEach((item, i) => {
-      if (i < parseInt(params.group)) {
-        stock = stock + item.projets.length-1;
-      }
+    let stock = [];
+    props.experiences.forEach((group, i) => {
+      group.projets.forEach((item, j) => {
+        if (i <= parseInt(params.group) && j <= parseInt(params.item)) {
+          stock = stock.concat(item);
+        }
+      })
     });
     setstep(stock);
-  }, [props, experiences, params])
+  }, [props, params])
   
   const pred = () => {
     let group = parseInt(params.group);
     let item = parseInt(params.item);
-    let limit = experiences[group-1] ? experiences[group-1].projets.length-1 : 0;
+    let limit = props.experiences[group-1] ? props.experiences[group-1].projets.length-1 : 0;
     if (item !== 0) {
       return "/experiences/"+group+"/"+(item-1);
     } else {
       return "/experiences/"+(group === 0 ? 0 : group-1)+"/"+limit;
     }
   }
+  
   const next = () => {
     let group = parseInt(params.group);
     let item = parseInt(params.item);
-    let limit = experiences[group].projets.length-1;
-    let overfow = experiences.length-1;
+    let limit = props.experiences[group].projets.length-1;
+    let overfow = props.experiences.length-1;
 
     if (item < limit) {
       return "/experiences/"+group+"/"+(item+1);
@@ -113,6 +44,7 @@ const Experiences = (props) => {
     }
     
   }
+
   return (
     <div className="container">
 
@@ -121,7 +53,7 @@ const Experiences = (props) => {
         <div className="big-title">Mes Expériences Professionelles</div>
         <div className="category">
           {
-            experiences.map((item, i)=>(
+            props.experiences.map((item, i)=>(
               <Link to={"/experiences/"+i+"/0"}
                 key={i} 
                 className={i === parseInt(params.group) ? "category-item-active" : "category-item"}
@@ -136,15 +68,15 @@ const Experiences = (props) => {
         <div className="caroussel">
           <div className="slide">
             <div className="polaroid shadow">
-              <div className="img" style={{backgroundImage: experiences[parseInt(params.group)].projets[parseInt(params.item)].img}}></div>
-              <div className="name">{experiences[parseInt(params.group)].projets[parseInt(params.item)].name}</div>
+              <div className="img" style={{backgroundImage: props.experiences[parseInt(params.group)].projets[parseInt(params.item)].img}}></div>
+              <div className="name">{props.experiences[parseInt(params.group)].projets[parseInt(params.item)].name}</div>
             </div>
 
             <div className="description">
-              <div className="title">{experiences[parseInt(params.group)].projets[parseInt(params.item)].name}</div>
-              <div className="text">{experiences[parseInt(params.group)].projets[parseInt(params.item)].description}</div>
-              <div className="number">{experiences[parseInt(params.group)].projets[parseInt(params.item)].date}</div>
-              <a href={experiences[parseInt(params.group)].projets[parseInt(params.item)].link} className="more"> Voir + </a>
+              <div className="title">{props.experiences[parseInt(params.group)].projets[parseInt(params.item)].name}</div>
+              <div className="text">{props.experiences[parseInt(params.group)].projets[parseInt(params.item)].description}</div>
+              <div className="number">{props.experiences[parseInt(params.group)].projets[parseInt(params.item)].date}</div>
+              <a href={props.experiences[parseInt(params.group)].projets[parseInt(params.item)].link} className="more"> Voir + </a>
             </div>
           </div>
 
@@ -155,9 +87,9 @@ const Experiences = (props) => {
             <i className="fas fa-arrow-left"></i> 
           </Link>
           <div className="passeur-number">
-            {step+1}
+            {step.length}
             {" / "}
-            {experiences.map(item => item.projets.length).reduce((total, num) => (total + num))}
+            {props.experiences.map(item => item.projets.length).reduce((total, num) => (total + num))}
           </div>
           <Link className="passeur-item" to={next()}> 
             <i className="fas fa-arrow-right"></i> 
@@ -172,7 +104,7 @@ const Experiences = (props) => {
         </div>
         <div className="details-list">
           {
-            experiences[parseInt(params.group)].projets.map((item, i)=>(
+            props.experiences[parseInt(params.group)].projets.map((item, i)=>(
               <Link to={"/experiences/"+parseInt(params.group)+"/"+i}
                 key={i} 
                 className={i === parseInt(params.item) ? "list-item list-item-active" : "list-item"}
